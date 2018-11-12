1535a1c15c8ff19011af21cfe2a422ca5ca62c4e
Arnaud RUFFAULT <ruffault.arnaud@gmail.com>
2018-11-12 19:58:11 +0100

    creation du projet, mise en place du routing, definition de l'url backend dans environnement.ts, installation de angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { TestComponent } from './test/test.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
